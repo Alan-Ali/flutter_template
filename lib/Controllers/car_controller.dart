556f4fcd7bab0ffc05c8700283129924c3aeb1c8
Alan-Ali <alaan376@gmail.com>
2023-06-09 14:09:02 +0300

    9th of june first commit

--- lib/Controllers/car_controller.dart
+++ lib/Controllers/car_controller.dart
@@ -3,9 +3,9 @@ import "../Models/cars.dart";
 import "./dependencies.dart";
 import "./rest_service.dart";
 
-
-class CarsController{
+class CarsController {
   final rest = service<RestService>();
+
   // static CarsController? _instance;
   //
   // CarsController._();
@@ -15,31 +15,31 @@ class CarsController{
   Future<List<Cars>> getCarList() async {
     final response = await rest.get("FlutterTestServlet");
 
-    List<Cars>  list = response.map((element) => Cars.fromJson(element)).toList();
+    List<Cars> list =
+        response.map((element) => Cars.fromJson(element)).toList();
 
     return list;
   }
 
-  // Future<List<Cars>> getUserTodoList(int? userId) async {
-  //     final todoList = await getCarList();
-  //     List<Cars>? filteredTodo = todoList.where((element) => element.userIdd == userId).toList();
+// Future<List<Cars>> getUserCarList(int? userId) async {
+//   final todoList = await getCarList();
+//   List<Cars>? filteredTodo =
+//       todoList.where((element) => element.userIdd == userId).toList();
+//
+//   return filteredTodo;
+// }
+//
+// Future<Cars> updateTodoList(int? id, Cars? todo) async {
+//   final response = await rest.patch("todos/$id", data: todo!);
+//
+//   return Cars.fromJson(response);
+// }
+//
+// Future createTodo(Cars? todo) async {
+//   await rest.post("todos/", todo: todo!);
+// }
   //
-  //      return filteredTodo;
-  //
-  // }
-  //
-  // Future<Cars> updateTodoList(int? id, Cars? todo) async {
-  //   final response =  await rest.patch("todos/$id", data: todo!);
-  //
-  //   return Cars.fromJson(response);
-  // }
-  //
-  // Future createTodo(Cars? todo) async {
-  //       await rest.post("todos/", todo: todo!);
-  // }
-  //
-  // Future deleteTodo(int id) async {
-  //   await rest.delete(id,"todos/$id");
-  // }
-
+// Future deleteTodo(int id) async {
+//   await rest.delete(id, "todos/$id");
+// }
 }
--- lib/Controllers/rest_service.dart
+++ lib/Controllers/rest_service.dart
@@ -1,4 +1,5 @@
 import "dart:convert";
+
 // ignore: import_of_legacy_library_into_null_safe
 import "package:flutter/foundation.dart";
 import "package:http/http.dart" as http;
@@ -12,79 +13,75 @@ var wifiBSSID = info.getWifiBSSID(); // 11:22:33:44:55:66
 var wifiIP = info.getWifiIP(); // 192.168.1.1
 var wifiName = info.getWifiName(); // FooNetwork
 
-
 class RestService {
-
   final String url = "http://localhost:8080/javaWebTests";
 
+  // remember to set the headers carefully, whether it's application/x-www-form-urlencoded, application/json or other types, to get the data back
   Future<List<dynamic>> get(String endpoint) async {
-      final response = await http.get(Uri.parse("$url/$endpoint"),headers: {"Content-Type": "application/json"});
+    final response = await http.get(Uri.parse("$url/$endpoint"),
+        headers: {"Content-Type": "application/x-www-form-urlencoded"});
 
-      if(response.statusCode == 200){
-        if (kDebugMode) {
-          print(response.body);
-        }
-        return jsonDecode(response.body);
-      } throw response;   
-  
+    if (response.statusCode == 200) {
+      if (kDebugMode) {
+        print(response.body);
+      }
+      return jsonDecode(response.body);
+    }
+    throw response;
   }
 
   Future<dynamic> post(String endpoint, {required Todo todo}) async {
     final response = await http.post(Uri.parse("$url/$endpoint"),
-    headers: {"Content-Type": "application/json"}, body: jsonEncode(todo));
+        headers: {"Content-Type": "application/json"}, body: jsonEncode(todo));
 
-    if(response.statusCode == 201 ){
+    if (response.statusCode == 201) {
       return jsonDecode(response.body);
-    }throw response;
-
+    }
+    throw response;
   }
 
   Future<dynamic> patch(String endpoint, {dynamic data}) async {
-    final response = await http.patch(Uri.parse("$url/$endpoint"), headers: {"Content-Type": "application/json"}, body: jsonEncode(data.toJson()) );
+    final response = await http.patch(Uri.parse("$url/$endpoint"),
+        headers: {"Content-Type": "application/x-www-form-urlencoded"},
+        body: jsonEncode(data.toJson()));
 
-    if(response.statusCode == 200){
-        return jsonDecode(response.body);
-    }throw response; 
-
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    }
+    throw response;
   }
 
-
-
-  Future<dynamic> delete(int id,String endpoint) async {
+  Future<dynamic> delete(int id, String endpoint) async {
     // final response = await http.delete("$url/$endpoint");
     final request = http.Request("DELETE", Uri.parse("$url/$endpoint"));
     request.body = jsonEncode({'id': id});
     request.headers.addAll({"Content-Type": "application/json"});
 
     final response = await request.send();
-    if(response.statusCode == 200){
+    if (response.statusCode == 200) {
       return print("delete Success");
-    }throw response;
-
-  } 
+    }
+    throw response;
+  }
 
   /* this will return the log of the */
   Future<dynamic> logs() async {
-      final response = await http.get(Uri.parse('$url/logs'), headers: {"Content-Type": "application/json"});
-      return jsonDecode(response.body); 
+    final response = await http.get(Uri.parse('$url/logs'),
+        headers: {"Content-Type": "application/x-www-form-urlencoded"});
+    return jsonDecode(response.body);
   }
-
 
   Future updateLog(int id) async {
     final response = await logs();
-    Logs json = Logs.fromJson(json:response);
+    Logs json = Logs.fromJson(json: response);
     json.log = json.log;
     json.lastLogin = id;
     await patch('logs', data: json);
   }
 
-  // Future<int> lastLog() async {
-  //   final response = await http.get('$url/logs');
+// Future<int> lastLog() async {
+//   final response = await http.get('$url/logs');
 
-  //   return jsonDecode(response.body);
-  // }
-
-
+//   return jsonDecode(response.body);
+// }
 }
-
-
